c5236bae2cff96e270cd0ecc5e1442c2334315b5
Stxrmm <116326992+Stxrmm@users.noreply.github.com>
2024-12-30 04:43:57 -0500

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Footer from "./Footer";
+import Food from "./Food";
+function App() {
+
+  return(
+    <>
+        <Header></Header>
+        <Food></Food>
+        <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return(
+        <footer>
+            <p>
+                {new Date().getFullYear()}
+                &copy; Your Website Name
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
